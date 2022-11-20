85a1499113b113f2d5c716b4557719378b5bc8be
Kirill Machnev <machnev999@gmail.com>
2022-11-20 19:07:24 +0300

    broken test

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -5,7 +5,7 @@ import static org.hamcrest.Matchers.*;
 
 public class PostmanEchoTest {
     @Test
-    public void shouldResultFromPost() {
+    void shouldResultFromPost() {
 
 // Given - When - Then
 // Предусловия
@@ -18,7 +18,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"))
+                .body("data", equalTo("some body wast told me"))
         ;
     }
 }
